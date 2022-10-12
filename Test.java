5c2b0d0b63a714dd0951de7a92556acaa735308c
sudhakararun <gsudhakar1998@gmail.com>
2022-10-12 08:21:58 +0530

    First Commit

---
+++ Test.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Test {
+	public static void main(String[] args) {
+
+		ArrayList<Integer> ar = new ArrayList<>();
+		for (int i = 1; i < 10; i++)
+			ar.add(i);
+
+		Consumer<Integer> cons = (i) -> {
+			System.out.println(i);
+			System.out.println("test");
+		};
+		
+		ar.forEach(cons);
+
+	}
+
+}
